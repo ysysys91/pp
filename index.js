c32ed71a9e30b15c782aa014b398f32a2b2996cc
ysysys91 <ys0414choiys14@gmail.com>
2026-04-24 10:25:31 +0900

    111

---
+++ index.js
@@ -0,0 +1,21 @@
+function showhtml() {
+  const img = document.getElementById('skillImage')
+  img.src = 'html.png'
+  img.style.display = 'block'
+}
+
+function showcss() {
+  const img = document.getElementById('skillImage')
+  img.src = 'css.png'
+  img.style.display = 'block'
+}
+
+function showjs() {
+  const img = document.getElementById('skillImage')
+  img.src = 'js.png'
+  img.style.display = 'block'
+}
+
+function hide() {
+  document.getElementById('skillImage').style.display = 'none'
+}
